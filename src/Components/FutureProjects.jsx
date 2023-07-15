844dd6fab89474abd9e830f5adf5fda686fc6155
Aditya Sen <adityasen419@gmail.com>
2023-07-15 10:03:17 +0530

    ok

--- src/Components/FutureProjects.jsx
+++ src/Components/FutureProjects.jsx
@@ -48,15 +48,11 @@ const FutureProjects = () => {
         modules={[Pagination]}
         className="mySwiper p-2"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {[1, 2, 3, 4].map((index) => (
+          <SwiperSlide key={index}>
+            <SkeletonCard />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -23,9 +23,9 @@ const Home = () => {
       <Hero />
       <div>
         <ul className="flex flex-col items-center w-full  px-4 lg:px-20 py-0 lg:py-3">
-          <li className=" lg:border-l-[2px] border-slate-100 h-[580%]  absolute z-[-1]  left-4 lg:left-20"></li>
-          <li className="lg:border-l-[2px] border-slate-100  left-[50%] h-[580%]  absolute z-[-1]"></li>
-          <li className="lg:border-l-[2px] border-slate-100 h-[580%] absolute z-[-1] right-4 lg:right-20"></li>
+          <li className=" lg:border-l-[2px] border-slate-100 h-[650%]  absolute z-[-1]  left-4 lg:left-20"></li>
+          <li className="lg:border-l-[2px] border-slate-100  left-[50%] h-[650%]  absolute z-[-1]"></li>
+          <li className="lg:border-l-[2px] border-slate-100 h-[650%] absolute z-[-1] right-4 lg:right-20"></li>
           <About />
           <UpcomingProjects />
           <ReleasedProjects />
