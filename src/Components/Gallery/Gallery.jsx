536fb81130ae274fada7203a5fc3500ed5602f91
Shivang <shivang.rawat843@gmail.com>
2023-08-07 11:38:42 +0530

    Gallery issue fix

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -5,6 +5,7 @@ import "react-photo-view/dist/react-photo-view.css";
 
 const Gallery = (props) => {
   const [gallery, setGallery] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -14,10 +15,16 @@ const Gallery = (props) => {
       const data = await response.json();
       console.log(data, " gallery =>>>");
       setGallery(data);
+      setLoading(false); 
     };
 
     fetchProducts();
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <h1 className="text-center pt-20 lg:pt-28 text-4xl lg:text-6xl tracking-[2px]  lg:tracking-[6px] pb-10 lg:pb-20">
@@ -28,84 +35,84 @@ const Gallery = (props) => {
           <div className="gallery__div_container">
             <div className="gallery__col">
               <div className="gallery__row1">
-                <PhotoView key={1} src={gallery?.img1}>
+                <PhotoView src={gallery?.img1}>
                   <img src={gallery?.img1} alt=""></img>
                 </PhotoView>
               </div>
               <div className="gallery__row2">
-                <PhotoView key={2} src={gallery?.img2}>
+                <PhotoView src={gallery?.img2}>
                   <img src={gallery?.img2} alt=""></img>
                 </PhotoView>
               </div>
             </div>
             <div className="gallery__col">
               <div className="gallery__row1">
-                <PhotoView key={3} src={gallery?.img3}>
+                <PhotoView src={gallery?.img3}>
                   <img src={gallery?.img3} alt=""></img>
                 </PhotoView>
               </div>
               <div className="gallery__row2">
-                <PhotoView key={4} src={gallery?.img4}>
+                <PhotoView src={gallery?.img4}>
                   <img src={gallery?.img4} alt=""></img>
                 </PhotoView>
               </div>
             </div>
             <div className="gallery__col">
               <div className="gallery__row1">
-                <PhotoView key={5} src={gallery?.img5}>
+                <PhotoView src={gallery?.img5}>
                   <img src={gallery?.img5} alt=""></img>
                 </PhotoView>
               </div>
               <div className="gallery__row2">
-                <PhotoView key={6} src={gallery?.img6}>
+                <PhotoView src={gallery?.img6}>
                   <img src={gallery?.img6} alt=""></img>
                 </PhotoView>
               </div>
             </div>
             <div className="gallery__col">
               <div className="gallery__row1">
-                <PhotoView key={7} src={gallery?.img7}>
+                <PhotoView src={gallery?.img7}>
                   <img src={gallery?.img7} alt=""></img>
                 </PhotoView>
               </div>
               <div className="gallery__row2">
-                <PhotoView key={8} src={gallery?.img8}>
+                <PhotoView src={gallery?.img8}>
                   <img src={gallery?.img8} alt=""></img>
                 </PhotoView>
               </div>
             </div>
             <div className="gallery__col">
               <div className="gallery__row1">
-                <PhotoView key={9} src={gallery?.img9}>
+                <PhotoView src={gallery?.img9}>
                   <img src={gallery?.img9} alt=""></img>
                 </PhotoView>
               </div>
               <div className="gallery__row2">
-                <PhotoView key={10} src={gallery?.img10}>
+                <PhotoView src={gallery?.img10}>
                   <img src={gallery?.img10} alt=""></img>
                 </PhotoView>
               </div>
             </div>
             <div className="gallery__col">
               <div className="gallery__row1">
-                <PhotoView key={11} src={gallery?.img11}>
+                <PhotoView src={gallery?.img11}>
                   <img src={gallery?.img11} alt=""></img>
                 </PhotoView>
               </div>
               <div className="gallery__row2">
-                <PhotoView key={12} src={gallery?.img12}>
+                <PhotoView src={gallery?.img12}>
                   <img src={gallery?.img12} alt=""></img>
                 </PhotoView>
               </div>
             </div>
             <div className="gallery__col">
               <div className="gallery__row1">
-                <PhotoView key={13} src={gallery?.img13}>
+                <PhotoView src={gallery?.img13}>
                   <img src={gallery?.img13} alt=""></img>
                 </PhotoView>
               </div>
               <div className="gallery__row2">
-                <PhotoView key={14} src={gallery?.img14}>
+                <PhotoView src={gallery?.img14}>
                   <img src={gallery?.img14} alt=""></img>
                 </PhotoView>
               </div>
