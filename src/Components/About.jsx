00f531c41f54fb1e1e14b20c8adcb9e3182305cf
Shivam Badoni <shivambadoni26@gmail.com>
2023-09-09 17:42:50 +0530

    change

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
     <div className="flex flex-col  justify-center pt-10 lg:pt-2 -mt-12 sm:mt-2 md:-mt-4 lg:-mt-36 xl:-mt-24 relative z-40  ">
       <div className="text-center flex flex-col items-center text-gray-600 leading-7">
         <h1 className="text-4xl tracking-wide lg:tracking-widest lg:text-7xl pb-4 pt-5 lg:pt-16 text-black">
-          ABOUT JSR Productions
+          ABOUT JSR Production House
         </h1>
 
         {/* for large */}
