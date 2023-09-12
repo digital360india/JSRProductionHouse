e4cf70a3a9ab4eba27f38cd5fc9a908e0ee8eaa8
Nakul Thakur <thakurnakul119@gmail.com>
2023-09-12 16:59:57 +0530

    animation

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -25,6 +25,7 @@ const Gallery = (props) => {
     return <div>Loading...</div>;
   }
 
+
   return (
     <>
       <h1 className="text-center pt-20 -mt-5 lg:pt-28 text-4xl lg:text-6xl tracking-[2px]  lg:tracking-[6px] pb-10 lg:pb-20">
@@ -35,47 +36,47 @@ const Gallery = (props) => {
             <div className=" flex  gap-3 w-[1800px]  h-[250px]        ">
                 <div className="  ">
                   <PhotoView src={gallery?.img1}> 
-                    <img src={gallery?.img1} className="h-[100%] w-[200px] rounded-md   border-2 border-white   "  alt="img1"></img>
+                    <img src={gallery?.img1} className="trans h-[100%] w-[200px] rounded-md   border-2 border-white   "  alt="img1"></img>
                   </PhotoView>
               </div>
               <div className= "">
                   <PhotoView src={gallery?.img3}>
-                    <img src={gallery?.img3} className=" h-[100%]  w-[200px] rounded-md  border-2 border-white  " alt="img3"></img>
+                    <img src={gallery?.img3} className=" trans h-[100%]  w-[200px] rounded-md  border-2 border-white  " alt="img3"></img>
                   </PhotoView>
                
               </div>
               <div className="">
               
                   <PhotoView src={gallery?.img5}>
-                    <img src={gallery?.img5} className=" h-[100%] w-[400px]  rounded-md border-2 border-white  " alt="img5"></img>
+                    <img src={gallery?.img5} className=" trans h-[100%] w-[400px]  rounded-md border-2 border-white  " alt="img5"></img>
                   </PhotoView>
                 
               </div>
               <div className="">
              
                   <PhotoView src={gallery?.img7}>
-                    <img src={gallery?.img7} className=" h-[100%] w-[200px] rounded-md  border-2 border-white  " alt="img7"></img>
+                    <img src={gallery?.img7} className=" trans h-[100%] w-[200px] rounded-md  border-2 border-white  " alt="img7"></img>
                   </PhotoView>
                 
               </div>
               <div className="">
                 
                   <PhotoView src={gallery?.img9}>
-                    <img src={gallery?.img9} className=" h-[100%] w-[200px] rounded-md   border-2 border-white  " alt="img9"></img>
+                    <img src={gallery?.img9} className=" trans h-[100%] w-[200px] rounded-md   border-2 border-white  " alt="img9"></img>
                   </PhotoView>
                
               </div>
               <div className="">
                 
                   <PhotoView src={gallery?.img11}>
-                    <img src={gallery?.img11} className=" h-[100%] w-[400px] rounded-md  border-2 border-white  " alt="img11"></img>
+                    <img src={gallery?.img11} className=" trans h-[100%] w-[400px] rounded-md  border-2 border-white  " alt="img11"></img>
                   </PhotoView>
                 
               </div>
               <div className="">
               
                   <PhotoView src={gallery?.img13}>
-                    <img src={gallery?.img13} className=" h-[100%] w-[200px] rounded-md   border-2 border-white  " alt="img13"></img>
+                    <img src={gallery?.img13} className=" trans h-[100%] w-[200px] rounded-md   border-2 border-white  " alt="img13"></img>
                   </PhotoView>
                 </div>
            
@@ -84,49 +85,49 @@ const Gallery = (props) => {
               <div className="   ">
               
                   <PhotoView src={gallery?.img2}>
-                    <img src={gallery?.img2} className=" h-[100%] w-[413px] rounded-md   border-2 border-white  " alt="img13"></img>
+                    <img src={gallery?.img2} className=" trans h-[100%] w-[413px] rounded-md   border-2 border-white  " alt="img13"></img>
                   </PhotoView>
                 
               </div>
               <div className="">
            
                   <PhotoView src={gallery?.img4}>
-                    <img src={gallery?.img4} className=" h-[100%] w-[195px] rounded-md   border-2 border-white  " alt="img13"></img>
+                    <img src={gallery?.img4} className=" trans h-[100%] w-[195px] rounded-md   border-2 border-white  " alt="img13"></img>
                   </PhotoView>
                 
               </div>
               <div className="">
                
                   <PhotoView src={gallery?.img6}>
-                    <img src={gallery?.img6} className=" h-[100%] w-[195px] rounded-md   border-2 border-white  " alt="img13"></img>
+                    <img src={gallery?.img6} className=" trans h-[100%] w-[195px] rounded-md   border-2 border-white  " alt="img13"></img>
                   </PhotoView>
                
               </div>
               <div className="">
                 
                   <PhotoView src={gallery?.img8}>
-                    <img src={gallery?.img8} className=" h-[100%] w-[200px] rounded-md    border-2 border-white  " alt="img13"></img>
+                    <img src={gallery?.img8} className=" trans h-[100%] w-[200px] rounded-md    border-2 border-white  " alt="img13"></img>
                   </PhotoView>
                 
               </div>
               <div className="">
                 
                   <PhotoView src={gallery?.img12}>
-                    <img src={gallery?.img12} className=" h-[100%] w-[400px] rounded-md   border-2 border-white  " alt="img13"></img>
+                    <img src={gallery?.img12} className=" trans h-[100%] w-[400px] rounded-md   border-2 border-white  " alt="img13"></img>
                   </PhotoView>
                
               </div>
               <div className="">
                
                   <PhotoView src={gallery?.img10}>
-                    <img src={gallery?.img10} className=" h-[100%] w-[200px] rounded-md   border-2 border-white  " alt="img13"></img>
+                    <img src={gallery?.img10} className=" trans h-[100%] w-[200px] rounded-md   border-2 border-white  " alt="img13"></img>
                   </PhotoView>
                 
               </div>
               <div className="">
                 
                   <PhotoView src={gallery?.img14}>
-                    <img src={gallery?.img14} className=" h-[100%] w-[200px] rounded-md   border-2 border-white  " alt="img13"></img>
+                    <img src={gallery?.img14} className=" trans h-[100%] w-[200px] rounded-md   border-2 border-white  " alt="img13"></img>
                   </PhotoView>
                 </div>
              
