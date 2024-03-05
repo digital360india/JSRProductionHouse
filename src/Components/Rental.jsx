6049e654814575fc25403ba1734dc24e63f7a627
shanuchauhan1211 <shanu1211chauhan@gmail.com>
2024-03-05 10:54:21 +0530

    rentals done

--- src/Components/Rental.jsx
+++ src/Components/Rental.jsx
@@ -12,27 +12,27 @@ const Rental = () => {
     {
       heading: "Professional cinema cameras, equipments and lenses",
       content:
-        "At JSR, we take immense pride in providing top-tier equipment to fuely…",
+        "At JSR, we take immense pride in providing top-tier equipment to fuelyour creative journey. Unleash your creativity with cutting-edge technology. Our lineup boasts the latest in camera technology, ensuring that you have the tools you need to capture exactly what you desire. At JSR, we offer professional cameras from renowned brands like Arri, Red, Sony FX and Venice series, Canon, and Lumix. These cameras are tailored to meet your demands for excellence. In addition to cameras, we provide a variety of high-quality lenses, including Ultra Prime, Master Prime, Supreme Prime, Signature Prime, Cooke S4i and S7i, Pancro Classic, Xeen Prime, CP2, CP3 high-speed lenses, Sigma lenses, and many others.",
     },
     {
       heading: "Professional lighting system and gears",
       content:
-        "We understand that lighting is a fundamental element in the art of fil…",
+        "We understand that lighting is a fundamental element in the art of filmmaking and can make or break the visual quality of a film. That's why we offer a comprehensive range of lighting equipment, along with a team of experienced lighting technicians who are dedicated to crafting the perfect ambiance for your project. Our production house provides filmmakers with top-notch professional lighting solutions, including systems from Arri, Film Gear, Godox, and Aputure. Our offerings range from PAR lighting and SkyPanels to small RGB kits and much more.",
     },
     {
       heading: "Renting The Studio",
       content:
-        "Are you in search of the perfect studio space for your creative endeav…",
+        "Are you in search of the perfect studio space for your creative endeavors? Look no further! JSR Production House is your one-stop solution. We offer professional color grading setups, VFX, Unreal Engine-based virtual production setups with anti-latency sensors and virtual walls, as well as music production setups. Our equipment range includes Pro Tools, Universal Audio, DaVinci Resolve, BaseLight, and other professional software and equipment.",
     },
     {
       heading: "Transport & logistics",
       content:
-        "At JSR Production House, we understand that seamless logistics are vit…",
+        "At JSR Production House, we understand that seamless logistics are vital for successful production. That's why we specialize in providing transportation solutions for every aspect of your creative project. Whether you need to move equipment or your team, we've got you covered. Our fleet includes a range of vehicles, from luxury options like sports cars and limousines to commercial choices such as SUVs, MUVs, vanity vans, production pickup trucks, buses, taxis, and generators.",
     },
     {
       heading: "Location & Accomodation",
       content:
-        "At JSR Production House, we understand that comfortable and convenient…",
+        "At JSR Production House, we understand that comfortable and convenient accommodations are essential for the success of any production. That's why we specialize in providing top-notch lodging options for you. From prime locations to hotels, resorts, clubs, pubs, and cafes, we offer a comprehensive range of options, all available under one roof.",
     },
   ];
 
