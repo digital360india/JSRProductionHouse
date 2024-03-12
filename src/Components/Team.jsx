63709f5f0f82e94d944163b4f080ba928f1b91ac
digital360india <139943711+digital360india@users.noreply.github.com>
2024-03-12 13:16:41 +0530

    Chairman.png

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -71,7 +71,7 @@ const Team = () => {
           </div>
 
           <div className="flex flex-col justify-center items-center lg:flex-row pb-10 lg:pb-40 ">
-            <img src={bods[1]?.img} className="w-[320px] md:w-[600px] lg:w-[430px] lg:h-[400px] xl:w-[650px] xl:h-[420px] object-contain"></img>
+            <img src="Chairman.png" className="w-[320px] md:w-[600px] lg:w-[430px] lg:h-[400px] xl:w-[650px] xl:h-[420px] object-contain"></img>
             <div className="md:mr-16 lg:ms-8 px-0 lg:pl-16 lg:leading-7 sm:ps-8 float-right justify-start pt-0 lg:pt-5 leading-0 w-[320px] md:w-[600px]">
               <h1
                 className=" text-2xl lg:text-5xl pt-5 lg:pb-5"
