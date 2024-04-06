14f7b8ea3140838dff46af1b401d81cf746f67f5
Himanshika Rawat <himanshirawat604@gmail.com>
2024-04-06 15:18:56 +0530

    ventures added

--- src/Components/Ventures.jsx
+++ src/Components/Ventures.jsx
@@ -6,11 +6,7 @@ const Ventures = () => {
       <h1 className="text-5xl md:text-5xl lg:text-7xl  tracking-[2px]  lg:tracking-[6px] pt-5 lg:pt-10">
         Our Ventures
       </h1>
-      {/* <p className="font2  py-2 lg:py-8 w-[320px] md:w-[550px] lg:w-[800px] lg:text-[14px] tracking-wide leading-7 text-gray-600 text-center ">
-        VHS media llp, JSR Production house, JSR Record label
-      </p> */}
-      <div className=" pt-10  flex flex-col md:flex-row justify-around">
-
+      <div className=" pt-10  flex flex-col md:hidden ">
         <div className="cursor-pointer flex gap-4 lg:gap-36 items-center ">
           <div className="hover:scale-125 duration-200">
             <a
@@ -50,10 +46,10 @@ const Ventures = () => {
               />
             </a>
           </div>
-          
         </div>
+
         <div className="cursor-pointer flex gap-4 lg:gap-36 items-center ">
-        <div className="hover:scale-125 duration-200">
+          <div className="hover:scale-125 duration-200">
             <a
               href="https://instagram.com/vhs_mediallp?igshid=MzRlODBiNWFlZA=="
               target="_blank"
@@ -67,7 +63,10 @@ const Ventures = () => {
           </div>
 
           <div className="hover:scale-125 duration-200">
-            <a href="https://instagram.com/jsrrecordlabel?igshid=MzRlODBiNWFlZA==" target="_blank">
+            <a
+              href="https://instagram.com/jsrrecordlabel?igshid=MzRlODBiNWFlZA=="
+              target="_blank"
+            >
               <img
                 src="https://res.cloudinary.com/dsea9eyps/image/upload/v1698737451/samples/z965k6iozmv6avww0q0s.png"
                 alt="JSR Record Regional"
@@ -75,9 +74,12 @@ const Ventures = () => {
               />
             </a>
           </div>
-       
+
           <div className="hover:scale-125 duration-200">
-            <a href="https://instagram.com/jsrrecordlabel?igshid=MzRlODBiNWFlZA==" target="_blank">
+            <a
+              href="https://instagram.com/jsrrecordlabel?igshid=MzRlODBiNWFlZA=="
+              target="_blank"
+            >
               <img
                 src="https://res.cloudinary.com/dsea9eyps/image/upload/v1698737408/samples/jydv1y0pssxb035vm8dt.png"
                 alt="JSR Record Label"
@@ -86,7 +88,86 @@ const Ventures = () => {
             </a>
           </div>
         </div>
+      </div>
 
+      <div className="hidden pt-10  md:flex  justify-between w-full">
+        <div className="hover:scale-125 duration-200">
+          <a
+            href="https://instagram.com/vhs_mediallp?igshid=MzRlODBiNWFlZA=="
+            target="_blank"
+          >
+            <img
+              src="https://res.cloudinary.com/dsea9eyps/image/upload/v1698737496/samples/ixoleqe9lfa47ciek9kr.png"
+              alt="JSR Record Label"
+              className="w-24 h-24 lg:w-40 lg:h-40 text-red-700"
+            />
+          </a>
+        </div>
+
+        <div className="hover:scale-125 duration-200">
+          <a
+            href="https://instagram.com/vhs_mediallp?igshid=MzRlODBiNWFlZA=="
+            target="_blank"
+          >
+            <img
+              src="TVWorks.png"
+              alt="JSR TV Works"
+              className="w-24 h-24 lg:w-40 lg:h-40 text-red-700"
+            />
+          </a>
+        </div>
+
+        <div className="hover:scale-125 duration-200">
+          <a
+            href="https://instagram.com/vhs_mediallp?igshid=MzRlODBiNWFlZA=="
+            target="_blank"
+          >
+            <img
+              src="Studios.png"
+              alt="JSR Studios"
+              className="w-24 h-24 lg:w-40 lg:h-40 text-red-700"
+            />
+          </a>
+        </div>
+
+        <div className="hover:scale-125 duration-200">
+          <a
+            href="https://instagram.com/vhs_mediallp?igshid=MzRlODBiNWFlZA=="
+            target="_blank"
+          >
+            <img
+              src="Indie.png"
+              alt="Think Indie Films"
+              className="w-24 h-24 lg:w-40 lg:h-40 text-red-700"
+            />
+          </a>
+        </div>
+
+        <div className="hover:scale-125 duration-200">
+          <a
+            href="https://instagram.com/jsrrecordlabel?igshid=MzRlODBiNWFlZA=="
+            target="_blank"
+          >
+            <img
+              src="https://res.cloudinary.com/dsea9eyps/image/upload/v1698737451/samples/z965k6iozmv6avww0q0s.png"
+              alt="JSR Record Regional"
+              className="w-24 h-24 lg:w-40 lg:h-40 text-red-700"
+            />
+          </a>
+        </div>
+
+        <div className="hover:scale-125 duration-200">
+          <a
+            href="https://instagram.com/jsrrecordlabel?igshid=MzRlODBiNWFlZA=="
+            target="_blank"
+          >
+            <img
+              src="https://res.cloudinary.com/dsea9eyps/image/upload/v1698737408/samples/jydv1y0pssxb035vm8dt.png"
+              alt="JSR Record Label"
+              className="w-24 h-24 lg:w-40 lg:h-40 text-red-700"
+            />
+          </a>
+        </div>
       </div>
     </>
   );
